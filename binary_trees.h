923a6f030c72aa5b113c6ee999e3aa8f27051f74
Adrien MILLOT <adrien.millot@gmail.com>
2021-07-21 09:42:39 +0200

    Post-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -31,6 +31,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *prmParent, int prmValue);
 int binary_tree_is_leaf(const binary_tree_t *prmNode);
 int binary_tree_is_root(const binary_tree_t *prmNode);
 binary_tree_t *binary_tree_node(binary_tree_t *prmParent, int prmValue);
+void binary_tree_postorder(const binary_tree_t *prmTree, void (*prmFunc)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
 
