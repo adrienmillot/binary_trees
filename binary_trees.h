73baaed0869a6b5666eeefa1837e1a10b303b045
Adrien MILLOT <adrien.millot@gmail.com>
2021-07-21 10:20:06 +0200

    Lowest common ancestor

--- binary_trees.h
+++ binary_trees.h
@@ -13,10 +13,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -24,6 +24,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *prmFirst, const binary_tree_t *prmSecond);
 int binary_tree_balance(const binary_tree_t *prmTree);
 void binary_tree_delete(binary_tree_t *prmTree);
 size_t binary_tree_depth(const binary_tree_t *prmTree);
