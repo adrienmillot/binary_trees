0c34a9f2a533d929ada2372ca54a94d7079cdd6e
Adrien MILLOT <adrien.millot@gmail.com>
2021-07-21 10:13:03 +0200

    Sibling

--- binary_trees.h
+++ binary_trees.h
@@ -41,6 +41,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 void binary_tree_postorder(const binary_tree_t *prmTree, void (*prmFunc)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif
