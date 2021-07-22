550f995bc13ca422323d25285171df180600114e
Adrien MILLOT <adrien.millot@gmail.com>
2021-07-22 19:05:42 +0200

    BST - Insert

--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,6 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *prmTree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+bst_t *bst_insert(bst_t **prmTree, int prmValue);
 
 #endif
